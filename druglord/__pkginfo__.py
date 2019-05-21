3fef8108f4e33462e05cd5ff8aef1960201e29fa
Bruno Werneck <brunovaulawerneck@gmail.com>
2019-05-21 15:30:52 -0300

    Fixing documentation

--- druglord/__pkginfo__.py
+++ druglord/__pkginfo__.py
@@ -2,7 +2,7 @@
 """package info"""
 info = {
     'name': 'DrugLord',
-    'version': '0.1.11',
+    'version': '0.1.12',
     'license': 'MIT',
     'copyright': 'Copyright (c) 2019 Bruno Vaula Werneck '
                  '<brunovaulawerneck@gmail.com>',
